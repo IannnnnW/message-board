19b2e00ecab08b358930de696dfeaa1abc437090
IannnnnW <ianwamai@gmail.com>
2024-07-30 17:34:10 +0300

    port issue correction

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ app.use((error, req, res, next) => {
 })
 
 
-const PORT = process.env.PORT | 3000
+const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Listening for requests on port: ${PORT}`))
