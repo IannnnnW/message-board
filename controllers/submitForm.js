ff76ce94c00c9860a3666d48785c87ec0dcf42eb
IannnnnW <ianwamai@gmail.com>
2024-07-30 15:24:20 +0300

    corrected error with /new route

--- controllers/submitForm.js
+++ controllers/submitForm.js
@@ -1,6 +1,7 @@
 const { messages } = require('../controllers/getMessagesController')
 
 exports.handleSubmit = (req, res, next) =>{
-    messages.push({user: req.body.name, message: req.body.message, added: new Date()})
+    messages.push({user: req.body.name, text: req.body.message, added: new Date()})
+    console.log(messages)
     res.redirect('/')
 }
--- routes/newRoute.js
+++ routes/newRoute.js
@@ -5,7 +5,7 @@ const router = express.Router()
 const form = require('../controllers/getForm')
 const submit = require('../controllers/submitForm')
 
-router.get("/create-message", form.returnForm)
+router.get("/", form.returnForm)
 router.post("/submit-message", submit.handleSubmit)
 
 module.exports = router
